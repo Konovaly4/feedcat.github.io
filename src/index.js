b2f777916cc82dd42b9ee146503f98147960aeed
Maxim Konovalov <maxkonovalov82@yandex.ru>
2020-07-27 01:03:14 +0300

    all is done but need to clean

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const cardFish = document.querySelector('#card-fish');
 const cardChicken = document.querySelector('#card-chicken');
 
 const fuaGra = new Card(cardFuaGra, cardText, dataFuaGra, color);
-const fish = new Card(cardFish,cardText, dataFish, color);
+const fish = new Card(cardFish, cardText, dataFish, color);
 const chicken = new Card(cardChicken, cardText, dataChicken, color);
 
 fuaGra.default();
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -14,11 +14,13 @@ export default class Card {
     this._cardBg = this._cardItem.querySelector('.card__bg');
     this._card = this._cardItem.querySelector('.card');
     this._cardProduct = this._cardItem.querySelector('.card__product');
+    this._cardTitle = this._cardItem.querySelector('.card__title');
     this._cardSubtitle = this._cardItem.querySelector('.card__subtitle');
     this._cardPortion = this._cardItem.querySelector('.card__description-portion');
     this._cardMouse = this._cardItem.querySelector('.card__description-mouse');
     this._cardWeight = this._cardItem.querySelector('.card__weight');
     this._cardWeightValue = this._cardItem.querySelector('.card__weight-value');
+    this._cardCat = this._cardItem.querySelector('.card__cat');
     this._cardNote = this._cardItem.querySelector('.card__note');
     this._cardLink = this._cardItem.querySelector('.card__note-link');
     this._cardNoteDot = this._cardItem.querySelector('.card__note-dot');
@@ -26,22 +28,25 @@ export default class Card {
 
   _setState() {
     this._render()
-    this._cardProduct.textContent = this._cardText.product;
     this._cardSubtitle.textContent = this._cardProductData.subtitle;
     this._cardPortion.textContent = this._cardProductData.portion;
     this._cardMouse.textContent = this._cardProductData.mouse;
     this._cardWeightValue.textContent = this._cardProductData.weight;
-    this._cardNote.textContent = this._cardText.noteDefault;
-    this._cardLink.textContent = this._cardText.noteDefaultLink;
   }
 
   default() {
     this._setState();
     this._removeEventListeners();
+    this._textEnabled();
+    this._cardProduct.textContent = this._cardText.product;
+    this._cardProduct.classList.remove('text-selected');
+    this._cardCat.classList.remove('element-disabled');
     this._cardFrame.style.background = `linear-gradient(135deg, transparent 30px, ${this._color.default} 0)`;
     this._cardWeight.style.backgroundColor = this._color.default;
     this._cardNote.style.color = this._color.defaultNote;
     this._cardLink.style.color = this._color.default;
+    this._cardNote.textContent = this._cardText.noteDefault;
+    this._cardLink.textContent = this._cardText.noteDefaultLink;
     if (this._cardLink.classList.contains('element-invisible')) {
       this._cardLink.classList.remove('element-invisible');
     };
@@ -62,6 +67,10 @@ export default class Card {
   selected() {
     this._setState();
     this._removeEventListeners();
+    this._textEnabled();
+    this._cardProduct.textContent = this._cardText.product;
+    this._cardProduct.classList.remove('text-selected');
+    this._cardCat.classList.remove('element-disabled');
     this._cardNote.textContent = this._cardProductData.note;
     this._cardLink.textContent = '';
     if (!this._cardLink.classList.contains('element-invisible')) {
@@ -81,11 +90,33 @@ export default class Card {
     this.selected();
     this._cardFrame.style.background = `linear-gradient(135deg, transparent 30px, ${this._color.selectedHover} 0)`;
     this._cardWeight.style.backgroundColor = this._color.selectedHover;
+    this._cardProduct.textContent = this._cardText.productSelectedHover;
+    this._cardProduct.classList.add('text-selected');
+  }
+
+  _textDisabled() {
+    this._cardProduct.classList.add('text-disabled');
+    this._cardTitle.classList.add('text-disabled');
+    this._cardSubtitle.classList.add('text-disabled');
+    this._cardPortion.classList.add('text-disabled');
+    this._cardMouse.classList.add('text-disabled');
+  }
+
+  _textEnabled() {
+    this._cardProduct.classList.remove('text-disabled');
+    this._cardTitle.classList.remove('text-disabled');
+    this._cardSubtitle.classList.remove('text-disabled');
+    this._cardPortion.classList.remove('text-disabled');
+    this._cardMouse.classList.remove('text-disabled')
   }
 
   disabled() {
     this._setState();
     this._removeEventListeners();
+    this._textDisabled();
+    this._cardProduct.textContent = this._cardText.product;
+    this._cardProduct.classList.remove('text-selected');
+    this._cardCat.classList.add('element-disabled');
     this._cardNote.textContent = `Печалька, ${this._cardProductData.subtitle} закончился.`;
     this._cardNote.style.color = this._color.disabledNote;
     this._cardLink.textContent = '';
@@ -118,15 +149,17 @@ export default class Card {
   }
 
   _setEventListeners() {
-    this._cardFrame.addEventListener('mouseover', this._stageHover);
-    this._cardFrame.addEventListener('mouseout', this._stageDefault);
-    this._cardFrame.addEventListener('click', this._stateToggle);
+    this._card.addEventListener('mouseover', this._stageDefault);
+    this._card.addEventListener('mouseout', this._stageHover);
+    this._card.addEventListener('click', this._stateToggle);
+    this._cardLink.addEventListener('click', this._stateToggle);
   }
 
   _removeEventListeners() {
-    this._cardFrame.removeEventListener('mouseover', this._stageHover);
-    this._cardFrame.removeEventListener('mouseout', this._stageDefault);
-    this._cardFrame.removeEventListener('click', this._stateToggle);
+    this._card.removeEventListener('mouseover', this._stageDefault);
+    this._card.removeEventListener('mouseout', this._stageHover);
+    this._card.removeEventListener('click', this._stateToggle);
+    this._cardLink.removeEventListener('click', this._stateToggle);
   }
 
 }
